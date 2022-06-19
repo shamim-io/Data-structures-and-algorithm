27928a9e20ea0bfe078c436235d763a71fd2dff0
Shamim Ahmed <theshamimahmed@gmail.com>
2022-06-19 19:02:53 +0530

    preorder traversal pushed

---
+++ src/Binary-Tree/Problems/Preorder-traversal.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+var preorderTraversal = function (root) {
+  if (root === null) return [];
+
+  let result = [],
+    current = root,
+    stack = [];
+
+  while (current || stack.length > 0) {
+    while (current) {
+      result.push(current.val);
+      stack.push(current);
+      current = current.left;
+    }
+    current = stack.pop();
+    current = current.right;
+  }
+  return result;
+};
